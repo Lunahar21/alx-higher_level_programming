e8aeafc862e16585e7166c4beb17439de708f6f1
“Lunahar21” <kindinewd@gmail.com>
2023-05-09 06:50:20 -0700

    positive_negative_or_zero

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,12 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+def islower(c):
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def pow(a, b):
+def fizzbuzz():
+listint_t *insert_node(listint_t **head, int number);
+
+#endif
