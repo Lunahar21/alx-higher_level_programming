a41f99b84cf959a3dce2a84cdccf73eb0ab21738
“Lunahar21” <kindinewd@gmail.com>
2023-05-09 01:33:35 -0700

    concate-edge

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 world_first_3 = world[:3]
-world_last_2 = world[-2:]
+world_last_2 = world[:-2]
 middle_world = world[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
