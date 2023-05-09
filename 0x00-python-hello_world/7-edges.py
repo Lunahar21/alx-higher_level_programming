98ca8e7e6b48a13a744d25ba92b3adee725d46dd
“Lunahar21” <kindinewd@gmail.com>
2023-05-09 13:19:38 -0700

    hot fix

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -4,5 +4,5 @@ world_first_3 = world[:3]
 world_last_2 = world[-2:]
 middle_world = world[1:-1]
 print("First 3 letters: {}".format(word_first_3))
-print(f"Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format (middle_word))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
