effc2908e602c6acceeccdbf1680c7695e69d600
“Lunahar21” <kindinewd@gmail.com>
2023-05-09 13:15:42 -0700

    hot fix

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 world_first_3 = world[:3]
 world_last_2 = world[-2:]
 middle_world = world[1:-1]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+print("First 3 letters: {}".format(word_first_3))
+print(f"Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format (middle_word))
