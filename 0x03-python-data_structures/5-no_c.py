992968f30f98ffac5844eb7f21c55bb3588c5069
“Lunahar21” <kindinewd@gmail.com>
2023-05-14 10:48:47 -0700

    no

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = ""
-    for i in range(len(my_string)):
-        if my_string[i] == 'C' or my_string[i] == 'c':
-            pass
-        else:
-            new_str += my_string[i]
+    for i in my_string:
+        if i is not 'C' and  i is not 'c':
+        
+            new_str += i
         return new_str
